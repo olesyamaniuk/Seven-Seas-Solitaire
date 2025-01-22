c90fd619c648e5d90364438996ca16fbe0fc0119
olesyamaniuk <maniukolesya222@gmail.com>
2025-01-22 16:32:16 +0200

    2

--- js/slick.js
+++ js/slick.js
@@ -1,6 +1,22 @@
 
 
 $(function () {
+  $('.advantages-list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: 'linear',
+    prevArrow: '.gallery-list-left',
+    nextArrow: '.gallery-list-right',
+  });
+});
+$(function () {
   $('.gallery__list').slick({
     infinite: true,
     slidesToShow: 1,
@@ -9,7 +25,7 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 3500,
     speed: 1000,
-    dots: false,
+    dots: true,
     centerMode: true,
     cssEase: 'linear',
     prevArrow: '.gallery-list-left',
